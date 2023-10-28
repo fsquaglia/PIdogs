943854e337f80d98d405ec02b0153f41fa280821
fsquaglia <fernandosquaglia@gmail.com>
2023-10-28 18:37:03 -0300

    deploy render

--- api/src/db.js
+++ api/src/db.js
@@ -2,22 +2,30 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY_RAILWAY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY_RAILWAY, DB_DEPLOY_RENDER } =
+  process.env;
 
 //!activar para usar conectando a localhost
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
 
-//! desactivar para apuntar a loccalhost
+//! deploy en railway
 // const sequelize = new Sequelize(DB_DEPLOY_RAILWAY, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
+
+//! deploy en render
+const sequelize = new Sequelize(DB_DEPLOY_RENDER, {
+  logging: false,
+  native: false,
+  dialectOptions: { ssl: { require: true } },
+});
 
 const basename = path.basename(__filename);
 
